8ead7f27544eccbb742ce04454bb613db9cd0b5a
sarvind7 <swathi1arvind@gmail.com>
2020-09-23 09:03:19 -0700

    fixed function for csv file

--- src/read_csv.py
+++ src/read_csv.py
@@ -3,13 +3,16 @@ import sys
 import os
 
 
-def readFile(file):
-    countylist = []
-    newspaperlist = []
+def read_in_counties_newspapers(file):
+    """ A function that takes in a file of counties and the
+    corresponding local newspaper. Then, stores the counties in
+    a list, and the local newspapers in another list and stores
+    the two lists in a tuple."""
+    county_list = []
+    newspaper_list = []
     with open(file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            countylist.append(row['counties'])
-            newspaperlist.append(row['localnewspapers'])
-    totallist = (countylist,newspaperlist)
-    return totallist
+            county_list.append(row['counties'])
+            newspaper_list.append(row['localnewspapers'])
+        return (county_list, newspaper_list)
--- test/test_read_csv.py
+++ test/test_read_csv.py
@@ -1,9 +1,9 @@
-from src.read_csv import readFile
+from src.read_csv import read_in_counties_newspapers
 def test_csv_with_one_county():
-    assert readFile('test/test_data/county_to_newspaper_test1.csv') == (["Los Angeles"],["https://www.latimes.com/"])
+    assert read_in_counties_newspapers('test/test_data/county_to_newspaper_test1.csv') == (["Los Angeles"],
+                                                                                           ["https://www.latimes.com/"])
 
 def test_csv_with_two_counties():
-    assert readFile('test/test_data/county_to_newspaper_test2.csv') == (["Los Angeles", "San Diego"],["https://www.latimes.com/", "https://www.sandiegouniontribune.com/"])
+    assert read_in_counties_newspapers('test/test_data/county_to_newspaper_test2.csv') == (["Los Angeles", "San Diego"],
+                                                                                           ["https://www.latimes.com/", "https://www.sandiegouniontribune.com/"])
 
-def test_csv_with_three_counties():
-    assert readFile('test/test_data/county_to_newspaper_test3.csv') == (["Los Angeles", "San Diego", "Orange"],["https://www.latimes.com/", "https://www.sandiegouniontribune.com/", "https://www.ocregister.com/"])
