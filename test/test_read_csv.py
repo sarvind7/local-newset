e76a9c4f4d2b470243056433b6fcac173931b324
sarvind7 <swathi1arvind@gmail.com>
2020-10-04 10:36:30 -0700

    updated testing of csv file

--- test/test_read_csv.py
+++ test/test_read_csv.py
@@ -1,7 +1,7 @@
 from src.read_csv import read_county_to_newspaper_csv
 def test_csv_with_one_county():
-    assert read_county_to_newspaper_csv('test/test_data/county_to_newspaper_test1.csv') == [("Los Angeles"),
-                                                                                            ("https://www.latimes.com/")]
+    assert read_county_to_newspaper_csv('test/test_data/county_to_newspaper_test1.csv') == [("Los Angeles",),
+                                                                                            ("https://www.latimes.com/",)]
 
 def test_csv_with_two_counties():
     assert read_county_to_newspaper_csv('test/test_data/county_to_newspaper_test2.csv') == [("Los Angeles", "San Diego"),
