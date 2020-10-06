262a343bbc16da67ceb49d17990f7d3580c5e845
sarvind7 <swathi1arvind@gmail.com>
2020-10-06 11:08:12 -0700

    read-csv in pairs of counties and newspapers

--- src/read_csv.py
+++ src/read_csv.py
@@ -6,13 +6,9 @@ import os
 def read_county_to_newspaper_csv(file):
     """ Takes in a csv file with each row containing a county
     and newspaper and outputs a list of (county, newspaper) tuples."""
-    county_list = []
     newspaper_list = []
     with open(file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            county_list.append(row['counties'])
-            newspaper_list.append(row['localnewspapers'])
-    county_tuple = tuple(county_list)
-    newspaper_tuple = tuple(newspaper_list)
-    return [county_tuple, newspaper_tuple]
+            newspaper_list.append((row['counties'], row['localnewspapers']))
+    return newspaper_list
